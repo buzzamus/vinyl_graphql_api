3724c5bb4194ac5c8f88f0418221abb81c62dd1b
Brent Busby <brent.busby@yahoo.com>
2019-11-26 06:49:30 -0600

    add user signin mutation, type

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,5 +4,6 @@ module Types
     field :create_album, mutation: Mutations::CreateAlbum
     field :create_artist, mutation: Mutations::CreateArtist
     field :create_user, mutation: Mutations::CreateUser
+    field :signin_user, mutation: Mutations::SignInUser
   end
 end
